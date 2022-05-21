44866572621bc865fa3d8ef0861e7643493cf709
lena falina <aternetas@gmail.com>
2022-05-21 16:34:46 +0300

    test commit3

---
+++ CliningContoraFromValera/CliningContoraFromValera.DB/dbo/Tables/Employee_Service.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Employee_Service] (
+    [Id]         INT IDENTITY (1, 1) NOT NULL,
+    [EmployeeId] INT NOT NULL,
+    [ServiceId]  INT NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC),
+    FOREIGN KEY ([EmployeeId]) REFERENCES [dbo].[Employee] ([Id]),
+    FOREIGN KEY ([ServiceId]) REFERENCES [dbo].[Service] ([Id])
+);
+
