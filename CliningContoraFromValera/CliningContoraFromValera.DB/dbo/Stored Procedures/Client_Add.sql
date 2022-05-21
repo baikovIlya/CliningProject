00c3a5fdbcc5197898e914d7ca799dc67a61a9e9
Maxim Bespalov <i.maksim_01@mail.ru>
2022-05-21 17:08:08 +0300

    add procedure Client_Add

---
+++ CliningContoraFromValera/CliningContoraFromValera.DB/dbo/Stored Procedures/Client_Add.sql
@@ -0,0 +1,29 @@
+﻿CREATE PROCEDURE [dbo].[Client_Add]
+	@Id int,
+	@FirstName nvarchar(50),
+	@LastName nvarchar(50),
+	@Email nvarchar(50),
+	@Phone nvarchar(25)
+AS
+BEGIN
+INSERT INTO Client(
+	FirstName,
+	LastName,
+	Email,
+	Phone)
+VALUES(
+	@FirstName,
+	@LastName,
+	@Email,
+	@Phone)
+
+SET @Id=SCOPE_IDENTITY()
+
+SELECT
+FirstName = @FirstName,
+LastName = @LastName,
+Email = @Email,
+Phone = @Phone
+FROM Client
+WHERE Id = @Id
+END
