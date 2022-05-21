9f096da9e4f789826db390083c410b2abc019ca9
Maxim Bespalov <i.maksim_01@mail.ru>
2022-05-21 17:16:59 +0300

    Add procedure Client_Update

---
+++ CliningContoraFromValera/CliningContoraFromValera.DB/dbo/Stored Procedures/Client_Update.sql
@@ -0,0 +1,18 @@
+﻿CREATE PROCEDURE [dbo].[Client_Update]
+	@Id int,
+	@FirstName nvarchar(50),
+	@LastName nvarchar(50),
+	@Email nvarchar(50),
+	@Phone nvarchar(25)
+
+AS
+BEGIN
+
+UPDATE Client
+SET FirstName = @FirstName,
+	LastName = @LastName,
+	Email = @Email,
+	Phone = @Phone
+WHERE Id = @Id
+
+END
