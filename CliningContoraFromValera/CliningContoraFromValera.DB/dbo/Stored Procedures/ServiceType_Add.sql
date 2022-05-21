cfbaf2aaf9b2dc2b04bf045d0ee09bcdc21c793c
Maxim Bespalov <i.maksim_01@mail.ru>
2022-05-22 01:36:03 +0300

    Add crud ServiceType_Add

---
+++ CliningContoraFromValera/CliningContoraFromValera.DB/dbo/Stored Procedures/ServiceType_Add.sql
@@ -0,0 +1,20 @@
+﻿CREATE PROCEDURE [dbo].[ServiceType_Add]
+	@Id int,
+	@Name nvarchar(50)
+
+AS
+BEGIN
+INSERT INTO [dbo].[ServiceType] (
+	Name
+	)
+VALUES (
+	@Name
+	)
+
+SET @Id = SCOPE_IDENTITY()
+
+SELECT 
+Name = @Name
+FROM [dbo].[ServiceType]
+WHERE Id=@Id
+END
