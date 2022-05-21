86510bda916b9fdab0876cd6e3d288b25130be62
Milana Maksina <m_maksinaa@mail.ru>
2022-05-21 23:08:06 +0300

    Add procedures GetById and Update for Order

---
+++ CliningContoraFromValera/CliningContoraFromValera.DB/dbo/Stored Procedures/Order_GetById.sql
@@ -0,0 +1,10 @@
+﻿CREATE PROCEDURE [dbo].[Order_GetById]
+	@Id int
+AS
+BEGIN
+
+	SELECT Id, ClientId, [Date], StartTime, EstimatedEndTime, EndTime, Price, [Status], AddressId, CountOfEmployees, IsCommercial
+	FROM dbo.[Order]
+	WHERE Id=@Id
+
+END
---
+++ CliningContoraFromValera/CliningContoraFromValera.DB/dbo/Stored Procedures/Order_Update.sql
@@ -0,0 +1,30 @@
+﻿CREATE PROCEDURE [dbo].[Order_Update]
+	@id int,
+	@ClientId int,
+	@Date nvarchar(10),
+	@StartTime nvarchar(10),
+	@EstimatedEndTime nvarchar(10),
+	@EndTime nvarchar(10),
+	@Price decimal(10,2),
+	@Status nvarchar(1),
+	@AddressId int,
+	@CountOfEmployees int,
+	@IsCommercial bit
+AS
+BEGIN
+
+UPDATE dbo.[Order]
+SET 
+        ClientId = @ClientId,
+        [Date] = @Date,
+        StartTime = @StartTime,
+        EstimatedEndTime = @EstimatedEndTime,
+        EndTime = @EndTime,
+        Price = @Price,
+        [Status] = @Status,
+        AddressId = @AddressId,
+        CountOfEmployees = @CountOfEmployees,
+        IsCommercial = @IsCommercial
+WHERE Id = @Id
+
+END
