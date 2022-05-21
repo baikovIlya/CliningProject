0900d8c4ac45a3537f45d4154c6ed330151ce3ec
Milana Maksina <m_maksinaa@mail.ru>
2022-05-21 21:36:29 +0300

    Add Order_Add

---
+++ CliningContoraFromValera/CliningContoraFromValera.DB/dbo/Stored Procedures/Order_Add.sql
@@ -0,0 +1,41 @@
+﻿CREATE PROCEDURE [dbo].[Order_Add]
+	@id int,
+	@ClientId int,
+	@Date nvarchar(10),
+	@StartTime nvarchar(10),
+	@EstimatedEndTime nvarchar(10),
+	@EndTime nvarchar(10),
+	@Price decimal(10,2),
+	@Status nvarchar(1),
+	@AddressId int,
+	@CountOfEmployees int,
+	@IsCommercial bit
+AS
+BEGIN
+INSERT INTO dbo.[Order](
+        Id,
+        ClientId,
+        [Date],
+        StartTime,
+        EstimatedEndTime,
+        EndTime,
+        Price,
+        [Status],
+        AddressId,
+        CountOfEmployees,
+        IsCommercial)
+        
+ VALUES (
+        @Id,
+        @ClientId,
+        @Date,
+        @StartTime,
+        @EstimatedEndTime,
+        @EndTime,
+        @Price,
+        @Status,
+        @AddressId,
+        @CountOfEmployees,
+        @IsCommercial)
+END
+ 
