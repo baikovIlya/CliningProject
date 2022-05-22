ef0309b9e890825a4760ff90b4580442804374f7
Maxim Bespalov <i.maksim_01@mail.ru>
2022-05-22 23:52:39 +0300

    +CRUD

---
+++ CliningContoraFromValera/CliningContoraFromValera.DB/dbo/Stored Procedures/Employee_Order_Add.sql
@@ -0,0 +1,15 @@
+﻿CREATE PROCEDURE [dbo].[Employee_Order_Add]
+	@Id int,
+	@EmployeeId int,
+	@OrderId int, 	
+AS
+BEGIN
+INSERT INTO dbo.Employee_Order(
+	EmployeeId,
+	OrderId)
+
+VALUES(
+	@EmployeeId,
+	@OrderId)
+SELECT @@IDENTITY
+END
