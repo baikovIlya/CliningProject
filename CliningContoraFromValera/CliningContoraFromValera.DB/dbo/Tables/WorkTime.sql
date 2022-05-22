b959a6f9ab89fd0896ca2d937507c4a57dd0a47a
ViStrashko <voringir@mail.ru>
2022-05-22 22:20:10 +0300

    Change time to nvarchar

--- CliningContoraFromValera/CliningContoraFromValera.DB/dbo/Tables/WorkTime.sql
+++ CliningContoraFromValera/CliningContoraFromValera.DB/dbo/Tables/WorkTime.sql
@@ -1,8 +1,8 @@
 ﻿CREATE TABLE [dbo].[WorkTime] (
     [Id]         INT           IDENTITY (1, 1) NOT NULL,
     [Date]       NVARCHAR (10) NOT NULL,
-    [StartTime]  TIME (7)      NOT NULL,
-    [FinishTime] TIME (7)      NULL,
+    [StartTime]  NVARCHAR (10) NOT NULL,
+    [FinishTime] NVARCHAR (10) NULL,
     PRIMARY KEY CLUSTERED ([Id] ASC)
 );
 
