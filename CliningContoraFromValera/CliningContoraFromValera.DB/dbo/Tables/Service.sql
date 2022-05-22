25180c8e02c83c1ec6e9b5ddb1e1099124d594f1
ViStrashko <voringir@mail.ru>
2022-05-22 22:26:03 +0300

    Change time to nvarchar

--- CliningContoraFromValera/CliningContoraFromValera.DB/dbo/Tables/Service.sql
+++ CliningContoraFromValera/CliningContoraFromValera.DB/dbo/Tables/Service.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [dbo].[Service] (
     [Id]              INT             IDENTITY (1, 1) NOT NULL,
     [Name]            NVARCHAR (100)  NOT NULL,
-    [Description]     NVARCHAR (255)  NULL,
+    [Description]     NVARCHAR (255)  NOT NULL,
     [Price]           DECIMAL (10, 2) NOT NULL,
     [CommercialPrice] DECIMAL (10, 2) NOT NULL,
     [Unit]            NVARCHAR (30)   NULL,
