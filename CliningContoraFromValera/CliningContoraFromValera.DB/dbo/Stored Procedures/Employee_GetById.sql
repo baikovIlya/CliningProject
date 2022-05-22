77f95f7cedfb1dc7167e89af4b42db3bac3ca511
lena falina <aternetas@gmail.com>
2022-05-22 14:02:37 +0300

    Add procedure Employee_GetById

---
+++ CliningContoraFromValera/CliningContoraFromValera.DB/dbo/Stored Procedures/Employee_GetById.sql
@@ -0,0 +1,13 @@
+﻿CREATE PROCEDURE [dbo].[Employee_GetById]
+	@Id int
+AS
+BEGIN
+	SELECT 
+		E.Id,
+		E.FirstName,
+		E.LastName,
+		E.Phone,
+		E.WorkTimeId
+	FROM dbo.[Employee] E
+	WHERE Id = @Id
+END	
