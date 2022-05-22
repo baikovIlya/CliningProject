743546d92778f740174942358329411b09cb13d0
ViStrashko <voringir@mail.ru>
2022-05-23 01:10:20 +0300

    Add RRUD procedure from Employee_WorkArea

---
+++ CliningContoraFromValera/CliningContoraFromValera.DB/dbo/Stored Procedures/Employee_WorkArea_Add.sql
@@ -0,0 +1,15 @@
+﻿CREATE PROCEDURE [dbo].[Employee_WorkArea_Add]
+	@Id int,
+	@WorkAreaId int,
+	@EmployeeId int
+AS
+BEGIN
+INSERT INTO dbo.Employee_WorkArea(
+	WorkAreaId,
+	EmployeeId)
+
+VALUES(
+	@WorkAreaId,
+	@EmployeeId)
+SELECT @@IDENTITY
+END
