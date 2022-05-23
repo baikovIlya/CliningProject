64e885f0a6cbb39b5154186b39c7a5a9c6a958f6
Milana Maksina <m_maksinaa@mail.ru>
2022-05-23 19:53:00 +0300

    Add GetAllEmployeeInfo

---
+++ CliningContoraFromValera/CliningContoraFromValera.DB/dbo/Stored Procedures/GetAllEmployeesInfo.sql
@@ -0,0 +1,8 @@
+﻿CREATE PROCEDURE [dbo].[GetAllEmployeesInfo]
+
+AS
+BEGIN
+	SELECT E.Id, E.FirstName, E.LastName, E.Phone, W.[Name] from dbo.[Employee] as E
+join dbo.[Employee_WorkArea] as WA on E.Id = WA.EmployeeId
+join dbo.[WorkArea] as W on WA.WorkAreaId = W.id
+END
