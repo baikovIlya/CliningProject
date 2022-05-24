fa62c5c8c11351da8ecad77d4aa5814399a3c4b1
Milana Maksina <m_maksinaa@mail.ru>
2022-05-24 15:15:20 +0300

    Add GetEmployeesSchedule

---
+++ CliningContoraFromValera/CliningContoraFromValera.DB/dbo/Stored Procedures/GetEmployeeSchedule.sql
@@ -0,0 +1,7 @@
+﻿CREATE PROCEDURE [dbo].[GetEmployeeSchedule]
+AS
+BEGIN
+	  SELECT E.Id, E.FirstName, E.LastName, WT.[Date], WT.StartTime, WT.FinishTime FROM [dbo].[Employee] AS E
+	  join dbo.WorkTime as WT on E.Id = WT.EmployeeId
+      WHERE E.IsDeleted = 0 
+END
