91322ef8cba28bd016466de14bc5a2d516e2e753
Milana Maksina <m_maksinaa@mail.ru>
2022-05-26 01:35:20 +0300

    Add GetAllServices

---
+++ CliningContoraFromValera/CliningContoraFromValera.DAL/ServiceManager.cs
@@ -0,0 +1,25 @@
+﻿using CliningContoraFromValera.DAL;
+using Dapper;
+using System.Data.SqlClient;
+using CliningContoraFromValera.DAL.DTOs;
+
+namespace CliningContoraFromValera.DAL
+{
+    public class ServiceManager
+    {
+        public string connectionString = @"Server=.;Database=CliningContoraFromValera.DB;Trusted_Connection=True;";
+
+        public List<ServiceDTO> GetAllServices()
+        {
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                return connection.Query<ServiceDTO>(
+                    StoredProcedures.Service_GetAll,
+                    commandType: System.Data.CommandType.StoredProcedure)
+                    .ToList();
+            }
+        }
+    }
+}
--- CliningContoraFromValera/ConsoleApp1/Program.cs
+++ CliningContoraFromValera/ConsoleApp1/Program.cs
@@ -1,14 +1,25 @@
 ﻿using CliningContoraFromValera.DAL;
+using CliningContoraFromValera.DAL.DTOs;
 
-var CL = new ClientManager();
+//var CL = new ClientManager();
 
-List<ClientDTO> test = CL.GetAllClients();
+//List<ClientDTO> test = CL.GetAllClients();
 
 //CL.AddClient(3, "малыш", "пупсович", "mfkssd", "+799992332");
 //CL.UpdateClientById(3, "гномик", "пупсочик", "вонючкин", "+792532342");
 //CL.DeleteClientById(3);
 
-foreach (var i in test)
+//foreach (var i in test)
+//{
+//    i.ToString();
+//    Console.WriteLine(i.ToString());
+//}
+
+var SM = new ServiceManager();
+
+List<ServiceDTO> service = SM.GetAllServices();
+
+foreach (var i in service)
 {
     i.ToString();
     Console.WriteLine(i.ToString());
