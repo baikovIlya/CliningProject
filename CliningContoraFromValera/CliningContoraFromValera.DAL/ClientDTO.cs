1c6acf08715baa6f6dbc9a43ec4b8f358d56bdc7
Milana Maksina <m_maksinaa@mail.ru>
2022-05-25 23:12:41 +0300

    Add ClientDTO and ClientManager

--- CliningContoraFromValera/CliningContoraFromValera.DAL/ClientDTO.cs
+++ CliningContoraFromValera/CliningContoraFromValera.DAL/ClientDTO.cs
@@ -6,7 +6,13 @@ using System.Threading.Tasks;
 
 namespace CliningContoraFromValera.DAL
 {
-    internal class ClientDTO
+    public class ClientDTO
     {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        
     }
 }
--- CliningContoraFromValera/CliningContoraFromValera.DAL/ClientManager.cs
+++ CliningContoraFromValera/CliningContoraFromValera.DAL/ClientManager.cs
@@ -1,12 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CliningContoraFromValera.DAL;
+using Dapper;
+using System.Data.SqlClient;
 
-namespace CliningContoraFromValera.DAL
+namespace CoCoCoWorking.DAL
 {
-    internal class ClientManager
+    public class ClientManager
     {
+        public string connectionString = @"Server=.;Database=CliningContoraFromValera.DB;Trusted_Connection=True;";
+
+        public List<ClientDTO> GetAllCustomers()
+        {
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                return connection.Query<ClientDTO>(
+                    "Client_GetAll",
+                    commandType: System.Data.CommandType.StoredProcedure)
+                    .ToList();
+            }
+        }
+
+        public ClientDTO GetClientByID(int id)
+        {
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                return connection.QuerySingle<ClientDTO>(
+                    "Client_GetById",
+                    param: new { id = id },
+                    commandType: System.Data.CommandType.StoredProcedure
+                    );
+            }
+        }
     }
 }
