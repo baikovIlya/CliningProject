6b223a1703766e4cbf4c8c9ac6ad977e30815af3
ViStrashko <voringir@mail.ru>
2022-05-25 21:52:39 +0300

    Add WorkArea_DeleteById

--- CliningContoraFromValera/CliningContoraFromValera.DB/dbo/Tables/WorkArea.sql
+++ CliningContoraFromValera/CliningContoraFromValera.DB/dbo/Tables/WorkArea.sql
@@ -1,6 +1,7 @@
 ﻿CREATE TABLE [dbo].[WorkArea] (
     [id]   INT IDENTITY (1, 1) NOT NULL,
     [Name] VARCHAR (30)        NOT NULL,
+    [IsDeleted] BIT DEFAULT 0  NOT NULL,
     PRIMARY KEY CLUSTERED ([id] ASC)
 );
 
