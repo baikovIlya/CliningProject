b8ccc9ac91cd93f42db4c417dd0f82c7616aa344
ViStrashko <voringir@mail.ru>
2022-05-26 03:31:43 +0300

    Add EmployeeDTO and EmployeeManager

--- CliningContoraFromValera/CliningContoraFromValera.DAL/EmployeeDTO.cs
+++ CliningContoraFromValera/CliningContoraFromValera.DAL/EmployeeDTO.cs
@@ -6,18 +6,16 @@ using System.Threading.Tasks;
 
 namespace CliningContoraFromValera.DAL
 {
-    public class ClientDTO
+    public class EmployeeDTO
     {
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public string? Email { get; set; }
         public string Phone { get; set; }
 
         public override string ToString()
         {
-            return $"Id={Id} FirstName={FirstName} LastName={LastName} Email={Email} Phone={Phone} ";
+            return $"Id={Id} FirstName={FirstName} LastName={LastName} Phone={Phone}";
         }
-
     }
 }
---
+++ CliningContoraFromValera/CliningContoraFromValera.DAL/EmployeeManager.cs
@@ -0,0 +1,91 @@
+﻿using Dapper;
+using System.Data.SqlClient;
+
+namespace CliningContoraFromValera.DAL
+{
+    public class EmployeeManager
+    {
+        public string connectionString = @"Server=.;Database=CliningContoraFromValera.DB;Trusted_Connection=True;";
+
+        public List<EmployeeDTO> GetAllEmployees()
+        {
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                return connection.Query<EmployeeDTO>(
+                    StoredProcedures.Employee_GetAll,
+                    commandType: System.Data.CommandType.StoredProcedure)
+                    .ToList();
+            }
+        }
+
+        public EmployeeDTO GetEmployeeByID(int id)
+        {
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                return connection.QuerySingle<EmployeeDTO>(
+                    StoredProcedures.Employee_GetById,
+                    param: new { id = id },
+                    commandType: System.Data.CommandType.StoredProcedure
+                    );
+            }
+        }
+
+        public void AddEmployee(int id, string firstName, string lastName, string phone)
+        {
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                connection.QuerySingle<EmployeeDTO>(
+                    StoredProcedures.Employee_Add,
+                    param: new
+                    {
+                        id = id,
+                        FirstName = firstName,
+                        LastName = lastName,
+                        Phone = phone
+                    },
+                    commandType: System.Data.CommandType.StoredProcedure
+                    );
+            }
+        }
+
+        public void UpdateEmployeeById(int id, string firstName, string lastName, string phone)
+        {
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                connection.QuerySingleOrDefault<EmployeeDTO>(
+                    StoredProcedures.Employee_UpdateById,
+                    param: new
+                    {
+                        id = id,
+                        FirstName = firstName,
+                        LastName = lastName,
+                        Phone = phone
+                    },
+                    commandType: System.Data.CommandType.StoredProcedure
+                    );
+            }
+        }
+
+        public void DeleteEmployeeById(int id)
+        {
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                connection.QuerySingleOrDefault<EmployeeDTO>(
+                    StoredProcedures.Employee_DeleteById,
+                    param: new { id = id },
+                    commandType: System.Data.CommandType.StoredProcedure
+                    );
+            }
+        }
+    }
+}
