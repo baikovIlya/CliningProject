e7a7784448ef6fa31a9570d937455f8c91d9e5ad
lena falina <aternetas@gmail.com>
2022-05-26 14:36:26 +0300

    add AddressDTO and AddressManager in .DAL

---
+++ CliningContoraFromValera/CliningContoraFromValera.DAL/DTOs/AddressDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace CliningContoraFromValera.DAL.DTOs
+{
+    public class AddressDTO
+    {
+        public int Id { get; set; }
+        public string Street { get; set; }
+        public string Bilding { get; set; }
+        public string Room { get; set; }
+        public int WorkAreaId { get; set; }
+    }
+}
