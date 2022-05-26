7ce476485f5e193db56a0bb2f80aaa70ae07ecd7
Maxim Bespalov <i.maksim_01@mail.ru>
2022-05-26 10:27:40 +0300

    add stored proc GetClientOrdersHistory

---
+++ CliningContoraFromValera/CliningContoraFromValera.DB/dbo/Stored Procedures/GetClientOrdersHistory.sql
@@ -0,0 +1,15 @@
+﻿CREATE PROCEDURE [dbo].[GetClientOrdersHistory]
+	@Id int
+
+AS
+BEGIN
+SELECT C.Id, C.FirstName, C.LastName, O.Id, ST.[Name], S.[Name], A.[Street], A.[Building], WA.[Name], O.Price FROM [dbo].Client AS C
+	join [dbo].[Order] as O on C.Id = O.ClientId
+	join [dbo].[Service_Order] as SO on O.Id = SO.OrderId
+	join [dbo].[Service] as S on SO.ServiceId = S.Id
+	join [dbo].[ServiceType] as ST on S.Id = ST.Id
+	join [dbo].[Address] as A on O.AddressId = A.Id
+	join [dbo].[WorkArea] as WA on A.WorkAreaId = WA.id
+
+	WHERE (C.Id = @Id AND C.IsDeleted = 0)
+END
