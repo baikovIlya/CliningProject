6783677cbc49ccab6d5e35d17402dc31c9de9ee6
baikovIlya <baikusha23@gmail.com>
2022-05-26 23:16:53 +0300

    Add procedure GetEmployyesAvailableForOrder

---
+++ CliningContoraFromValera/CliningContoraFromValera.DB/dbo/Stored Procedures/GetEmployyesAvailableForOrder.sql
@@ -0,0 +1,14 @@
+﻿CREATE PROCEDURE [dbo].[GetEmployyesAvailableForOrder]
+@Date nvarchar (10),
+@ServiceId int,
+@WorkAreaId int
+AS
+BEGIN
+	  SELECT Empl.FirstName, Empl.LastName, WT.StartTime, WT.FinishTime, Ser.[Name], Empl.Phone FROM [dbo].[Employee] AS Empl
+	  join dbo.WorkTime as WT on Empl.Id = WT.EmployeeId
+	  join dbo.Employee_Service as EmpSer on Empl.Id = EmpSer.EmployeeId
+	  join dbo.[Service] as Ser on EmpSer.ServiceId = Ser.Id
+	  join dbo.Employee_WorkArea as EmpArea on Empl.Id = EmpArea.EmployeeId
+	  join dbo.WorkArea as WAr on EmpArea.WorkAreaId = WAr.id
+      WHERE Empl.IsDeleted = 0 AND WT.[Date] = @Date AND Ser.Id = @ServiceId AND WAr.id = @WorkAreaId
+END
