5bf0606f677624a0c5a3cf5f295d1908a3dec72d
ViStrashko <voringir@mail.ru>
2022-05-26 03:02:39 +0300

    Add WorkAreaDTO and WorkAreaManager

---
+++ CliningContoraFromValera/CliningContoraFromValera.DAL/WorkAreaManager.cs
@@ -0,0 +1,87 @@
+﻿using Dapper;
+using System.Data.SqlClient;
+
+namespace CliningContoraFromValera.DAL
+{
+    public class WorkAreaManager
+    {
+        public string connectionString = @"Server=.;Database=CliningContoraFromValera.DB;Trusted_Connection=True;";
+
+        public List<WorkAreaDTO> GetAllWorkAreas()
+        {
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                return connection.Query<WorkAreaDTO>(
+                    StoredProcedures.WorkArea_GetAll,
+                    commandType: System.Data.CommandType.StoredProcedure)
+                    .ToList();
+            }
+        }
+
+        public WorkAreaDTO GetWorkAreaByID(int id)
+        {
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                return connection.QuerySingle<WorkAreaDTO>(
+                    StoredProcedures.WorkArea_GetById,
+                    param: new { id = id },
+                    commandType: System.Data.CommandType.StoredProcedure
+                    );
+            }
+        }
+
+        public void AddWorkArea(int id, string Name)
+        {
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                connection.QuerySingle<WorkAreaDTO>(
+                    StoredProcedures.WorkArea_Add,
+                    param: new
+                    {
+                        id = id,
+                        Name = Name
+                    },
+                    commandType: System.Data.CommandType.StoredProcedure
+                    );
+            }
+        }
+
+        public void UpdateClientById(int id, string Name)
+        {
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                connection.QuerySingleOrDefault<WorkAreaDTO>(
+                    StoredProcedures.WorkArea_UpdateById,
+                    param: new
+                    {
+                        id = id,
+                        Name = Name,
+                    },
+                    commandType: System.Data.CommandType.StoredProcedure
+                    );
+            }
+        }
+
+        public void DeleteClientById(int id)
+        {
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                connection.QuerySingleOrDefault<WorkAreaDTO>(
+                    StoredProcedures.WorkArea_DeleteById,
+                    param: new { id = id },
+                    commandType: System.Data.CommandType.StoredProcedure
+                    );
+            }
+        }
+    }
+}
