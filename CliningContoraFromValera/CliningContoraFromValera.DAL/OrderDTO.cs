7385fd31fe8e540c4e61e48f2d51c7ac2e07bb94
Maxim Bespalov <i.maksim_01@mail.ru>
2022-05-26 13:10:28 +0300

    add OrderDTO, need edit type

---
+++ CliningContoraFromValera/CliningContoraFromValera.DAL/OrderDTO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CliningContoraFromValera.DAL
+{
+    public class OrderDTO
+    {
+        public int Id { get; set; }
+        public string Date { get; set; }
+        public string StartTime { get; set; }
+        public string EstimatedEndTime { get; set; }
+        public string EndTime { get; set; }
+        public decimal SummOfOrder { get; set; }
+        public string Status { get; set; }
+        public int CountOfEmployees { get; set; }
+        public bool IsCommercial { get; set; }
+        public int ClientId { get; set; }
+        public int AddressId { get; set; }
+    }
+}
