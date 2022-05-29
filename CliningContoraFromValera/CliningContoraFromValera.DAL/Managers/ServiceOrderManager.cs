03d45685ee5545859956d98733df912fe1585073
Milana Maksina <m_maksinaa@mail.ru>
2022-05-29 15:17:54 +0300

    Add ServiceOrderManager

---
+++ CliningContoraFromValera/CliningContoraFromValera.DAL/Managers/ServiceOrderManager.cs
@@ -0,0 +1,83 @@
+﻿using CliningContoraFromValera.DAL.DTOs;
+using Dapper;
+using System.Data.SqlClient;
+
+namespace CliningContoraFromValera.DAL.Managers
+{
+    public class ServiceOrderManager
+    {
+        public List<ServiceOrderDTO> GetAllOrderInfo()
+        {
+            using (var connection = new SqlConnection(ServerSettings._connectionString))
+            {
+                connection.Open();
+
+                Dictionary<int, ServiceOrderDTO> result = new Dictionary<int, ServiceOrderDTO>();
+
+                connection.Query<ServiceOrderDTO, OrderDTO, ServiceDTO, ServiceOrderDTO>(
+                    "GetAllOrderServicesInfo",
+                    (serviceOrder, order, service) => {
+                        if (!result.ContainsKey(serviceOrder.Id))
+                        {
+                            result.Add(serviceOrder.Id, serviceOrder);
+                        }
+
+                        ServiceOrderDTO crnt = result[serviceOrder.Id];
+
+                        if (order != null)
+                        {
+                            crnt.Orders.Add(order);
+                        }
+                        if (service != null)
+                        {
+                            crnt.Services.Add(service);
+                        }
+
+                        return crnt;
+                    },
+                    commandType: System.Data.CommandType.StoredProcedure,
+                    splitOn: "Id"
+                );
+
+                return result.Values.ToList();
+            }
+        }
+
+        //public ServiceOrderDTO GetById(int id)
+        //{
+        //    using (var connection = new SqlConnection(ServerSettings._connectionString))
+        //    {
+        //        connection.Open();
+
+        //        Dictionary<int, ServiceOrderDTO> result = new Dictionary<int, ServiceOrderDTO>();
+
+        //        connection.Query<ServiceOrderDTO, OrderDTO, ServiceDTO, ServiceOrderDTO>(
+        //            "GetAllOrderServicesInfo",
+        //            (serviceOrder, order, service) => {
+        //                if (!result.ContainsKey(serviceOrder.Id))
+        //                {
+        //                    result.Add(serviceOrder.Id, serviceOrder);
+        //                }
+
+        //                ServiceOrderDTO crnt = result[serviceOrder.Id];
+
+        //                if (order != null)
+        //                {
+        //                    crnt.Orders.Add(order);
+        //                }
+        //                if (service != null)
+        //                {
+        //                    crnt.Services.Add(service);
+        //                }
+
+        //                return crnt;
+        //            },
+        //            commandType: System.Data.CommandType.StoredProcedure,
+        //            splitOn: "Id"
+        //        ).FirstOrDefault(); 
+
+        //    }
+        }
+
+    }
+}
