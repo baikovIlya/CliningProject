201f89a2f98a330ee934ebdafe77c85d70b0200d
Maxim Bespalov <i.maksim_01@mail.ru>
2022-06-02 22:39:03 +0300

    Add WorkTimeModelManager, +GetAll, +GetWorkTimeById

---
+++ CliningContoraFromValera/CliningContoraFromValera.Bll/ModelsManager/WorkTimeModelManager.cs
@@ -0,0 +1,24 @@
+﻿using CliningContoraFromValera.Bll.Models;
+using CliningContoraFromValera.DAL;
+using CliningContoraFromValera.DAL.Managers;
+using CliningContoraFromValera.DAL.DTOs;
+
+namespace CliningContoraFromValera.Bll.ModelsManager
+{
+    public class WorkTimeModelManager
+    {
+        WorkTimeManager WorkTimeManager = new WorkTimeManager();
+
+        public List<WorkTimeModel> GetAll()
+        {
+            List<WorkTimeDTO> workTimes = WorkTimeManager.GetAllWorkTimes();
+            return MapperConfigStorage.GetInstance().Map<List<WorkTimeModel>>(workTimes);
+        }
+
+        public WorkTimeModel GetWorkTimeById(int workTimeId)
+        {
+            WorkTimeDTO workTime = WorkTimeManager.GetWorkTimeById(workTimeId);
+            return MapperConfigStorage.GetInstance().Map<WorkTimeModel>(workTime);
+        }
+    }
+}
