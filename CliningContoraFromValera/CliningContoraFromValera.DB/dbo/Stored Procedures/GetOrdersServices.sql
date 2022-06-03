35c1f36d39c7ae081b4df5c1d7d24a54de623889
Milana Maksina <m_maksinaa@mail.ru>
2022-06-03 15:52:24 +0300

    Add GetOrdersServices

---
+++ CliningContoraFromValera/CliningContoraFromValera.DB/dbo/Stored Procedures/GetOrdersServices.sql
@@ -0,0 +1,11 @@
+﻿CREATE PROCEDURE [dbo].[GetOrdersServices]
+AS
+BEGIN
+SELECT O.Id, O.[Date], O.StartTime, O.EstimatedEndTime, O.FinishTime, O.Price, O.[Status], 
+O.CountOfEmployees, O.IsCommercial,
+S.Id, S.ServiceType, S.[Name], S.[Description], S.Price, S.CommercialPrice, S.Unit, S.EstimatedTime, 
+ SO.Id, SO.[Count]
+from dbo.[Order] as O
+join dbo.Service_Order as SO on O.Id = SO.OrderId
+join dbo.[Service] as S on SO.ServiceId = S.Id
+END
