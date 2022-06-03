78253e5e5df672d6f875e5c20789f4ab3ea44ed1
baikovIlya <baikusha23@gmail.com>
2022-06-03 21:50:47 +0300

    Add UITestElements.cs

---
+++ CliningContoraFromValera/CliningContoraFromValera.UI/UITextElements.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CliningContoraFromValera.UI
+{
+    public class UITextElements
+    {
+        public const string FirstName = "Имя";
+        public const string LastName = "Фамилия";
+        public const string PhoneNomer = "Телефон";
+        public const string Email = "Почта";
+        public const string EmptyFieldsError = "Все поля обязательны к заполнению!";
+    }
+}
