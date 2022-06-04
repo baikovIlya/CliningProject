0ac84c2ad18bb6942b3b483020fa82f15a5fb282
Sofa <sofarmakovski@gmail.com>
2022-06-04 19:55:27 +0300

    add ServiceOrderModelManager

---
+++ CliningContoraFromValera/CliningContoraFromValera.Bll/ModelsManager/ServiceOrderModelManager.cs
@@ -0,0 +1,16 @@
+﻿using CliningContoraFromValera.Bll.Models;
+using CliningContoraFromValera.DAL.Managers;
+using CliningContoraFromValera.DAL.DTOs;
+
+namespace CliningContoraFromValera.Bll.ModelsManager
+{
+    public class ServiceOrderModelManager
+    {
+        ServiceOrderManager _serviceOrderManager = new ServiceOrderManager();
+
+        public void AddServiceToOrder(ServiceOrderModel serviceOrderModel)
+        {
+            
+        }
+    }
+}
