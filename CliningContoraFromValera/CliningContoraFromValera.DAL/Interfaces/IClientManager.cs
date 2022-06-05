2d5c57ff4b987c487ddde819eda09b0e4785e495
lena falina <aternetas@gmail.com>
2022-06-05 21:00:29 +0300

    add IClientManager & IClientModelManager

---
+++ CliningContoraFromValera/CliningContoraFromValera.DAL/Interfaces/IClientManager.cs
@@ -0,0 +1,16 @@
+﻿using CliningContoraFromValera.DAL.DTOs;
+using System.Collections.Generic;
+
+
+namespace CliningContoraFromValera.DAL.Interfaces
+{
+    public interface IClientManager
+    {
+        List<ClientDTO> GetAllClients();
+        ClientDTO GetClientByID(int clientId);
+        void AddClient(ClientDTO newClient);
+        void UpdateClientById(ClientDTO newClient);
+        void DeleteClientById(int clientId);
+
+    }
+}
