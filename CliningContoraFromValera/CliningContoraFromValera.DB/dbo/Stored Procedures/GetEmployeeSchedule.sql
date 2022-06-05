6d859acc2f5300fe37ea46397e671c81e48d867d
Milana Maksina <m_maksinaa@mail.ru>
2022-06-05 14:35:32 +0300

    Fix GetEmployeeSchedule

--- CliningContoraFromValera/CliningContoraFromValera.DB/dbo/Stored Procedures/GetEmployeeSchedule.sql
+++ CliningContoraFromValera/CliningContoraFromValera.DB/dbo/Stored Procedures/GetEmployeeSchedule.sql
@@ -6,5 +6,5 @@ BEGIN
 	  SELECT E.Id, E.FirstName, E.LastName, E.Phone, WT.Id, WT.[Date], WT.StartTime, WT.FinishTime,
 	  WT.EmployeeId FROM [dbo].[Employee] AS E
 	  join dbo.WorkTime as WT on E.Id = WT.EmployeeId
-      WHERE E.IsDeleted = 0 AND WT.[Date] >= @MinDate AND WT.[Date] <= @MaxDate AND @MaxDate = @MinDate
+      WHERE E.IsDeleted = 0 AND WT.[Date] >= @MinDate AND WT.[Date] <= @MaxDate 
 END
