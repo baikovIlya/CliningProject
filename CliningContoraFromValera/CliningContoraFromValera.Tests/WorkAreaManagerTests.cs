fba85f01bcf95d33d19d7f8f7638078d05fdcd21
Maxim Bespalov <i.maksim_01@mail.ru>
2022-06-09 14:38:28 +0300

    add interface WorkArea and WorkAreaManagerTests

---
+++ CliningContoraFromValera/CliningContoraFromValera.Tests/WorkAreaManagerTests.cs
@@ -0,0 +1,13 @@
+﻿using CliningContoraFromValera.Bll.ModelsManager;
+using CliningContoraFromValera.DAL.Managers.ManagersInterfaces;
+using NUnit.Framework;
+using Moq;
+using CliningContoraFromValera.Bll.Models;
+using CliningContoraFromValera.DAL.DTOs;
+
+namespace CliningContoraFromValera.Tests
+{
+    public class WorkAreaManagerTests
+    {
+    }
+}
