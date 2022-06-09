7c8f44b024f047d369368d093ccbbc6d8a64e0de
Maxim Bespalov <i.maksim_01@mail.ru>
2022-06-09 12:57:06 +0300

    add override equals

--- CliningContoraFromValera/CliningContoraFromValera.Bll/Models/WorkAreaModel.cs
+++ CliningContoraFromValera/CliningContoraFromValera.Bll/Models/WorkAreaModel.cs
@@ -4,14 +4,35 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
+
+        public WorkAreaModel()
+        {
+
+        }
+
         public override string ToString()
         {
             return Name;
         }
 
-        public WorkAreaModel()
+        public override bool Equals(object? obj)
         {
+            bool flag = false;
 
+            if(obj == null || !(obj is WorkAreaModel))
+            {
+                flag = false;
+            }
+            else
+            {
+                WorkAreaModel waDto = (WorkAreaModel)obj;
+                if (waDto.Id != this.Id || waDto.Name != this.Name)
+                {
+                    flag = false;
+                }
+            }
+
+            return flag;
         }
     }
 }
