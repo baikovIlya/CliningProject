d96ec270d984846aa829afdd73fe3af5b02cb7cc
Maxim Bespalov <i.maksim_01@mail.ru>
2022-06-09 18:02:47 +0300

    delete interface modelmanager

--- CliningContoraFromValera/CliningContoraFromValera.Tests/WorkAreaManagerTestSources.cs
+++ CliningContoraFromValera/CliningContoraFromValera.Tests/WorkAreaManagerTestSources.cs
@@ -1,12 +1,59 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CliningContoraFromValera.Bll.Models;
+using CliningContoraFromValera.DAL.DTOs;
 
 namespace CliningContoraFromValera.Tests
 {
-    public class WorkAreaManagerTestSources
+    public static class WorkAreaModelsTest
     {
+        public static List<WorkAreaModel> workAreaModels = new List<WorkAreaModel>()
+        {
+            new WorkAreaModel()
+            {
+                Id = 1,
+                Name = "Moscovskiy"
+            },
+
+            new WorkAreaModel()
+            {
+                Id = 2,
+                Name = "Frunzenskiy"
+            }
+        };
+    }
+
+    public static class WorkAreaDtosTest
+    {
+        public static List<WorkAreaDTO> workAreaDtos = new List<WorkAreaDTO>()
+        {
+            new WorkAreaDTO()
+            {
+                Id = 1,
+                Name = "Moskovskiy"
+            },
+
+            new WorkAreaDTO()
+            {
+                Id = 2,
+                Name = "Frunzenskiy"
+            }
+        };
+    }
+
+    public class WorkAreaManagerTestSources : IEnumerable
+    {
+        public IEnumerator GetEnumerator()
+        {
+            yield return new object[]
+            {
+                WorkAreaModelsTest.workAreaModels[0],
+                WorkAreaDtosTest.workAreaDtos[0]
+            };
+        }
     }
 }
