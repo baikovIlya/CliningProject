82eb78f83146e4af8a988c36dda21b788aa71eac
lena falina <aternetas@gmail.com>
2022-06-10 11:50:08 +0300

    ...

--- CliningContoraFromValera/CliningContoraFromValera.Bll/Models/OrderModel.cs
+++ CliningContoraFromValera/CliningContoraFromValera.Bll/Models/OrderModel.cs
@@ -11,13 +11,7 @@
         public StatusType Status { get; set; }
         public int CountOfEmployees { get; set; }
         public bool IsCommercial { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Phone { get; set; }
-        public string Name { get; set; }
-        public string Street { get; set; }
-        public string Building { get; set; }
-        public string Room { get; set; }
+
         public ClientModel Client { get; set; }
         public AddressModel Address { get; set; }
 
